12c6965293b475854ba44ae088799d4f82f3ccd8
Z. Safiy Errahmane <Z. Safiy Errahmane>
2017-03-18 21:55:08 +0100

    just learning JQuery

--- header.php
+++ header.php
@@ -1,21 +1,23 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-	<title>tite du site</title>
-	<meta charset="utf-8"/>
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<link rel="stylesheet" type="text/css" href="css/main.css">
-	<meta charset="utf-8">
+    <title>tite du site</title>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="script/test.js"></script>
 </head>
 
 <div class="header">
-	
-	<div class="logo"> 
-			<h1> SITEname.com</h1>
-	</div>
-	<div class="lien">
-		<a href="#admin">admin</a>
-		<a href="#login">login</a>
-	</div>
+
+    <div class="logo">
+        <h1> SITEname.com</h1>
+    </div>
+    <div class="lien">
+        <a href="#admin">admin</a>
+        <a href="#login">login</a>
+    </div>
 
 </div>
