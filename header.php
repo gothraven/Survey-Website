a44486415ffc941b769129c458c533614e72205e
NOUKAZA <noukaza@localhost.localdomain>
2017-03-18 19:41:43 +0100

    test

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>tite du site</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<meta charset="utf-8">
+</head>
+
+<div class="header">
+	
+	<div class="logo"> 
+			<h1> SITEname.com</h1>
+	</div>
+	<div class="lien">
+		<a href="#admin">admin</a>
+		<a href="#login">login</a>
+	</div>
+</div>
+	
+
---
+++ index.php
@@ -0,0 +1,36 @@
+<?
+require 'header.php';
+?>
+<div class="contenu">
+	 <div class="question">
+	 		<h1>titre </h1>
+	 		<p>tesxt  text texttexttext text text text text text text</p>
+
+	 </div>
+	 <div class="question">
+	 		<h1>titre </h1>
+	 		<p>tesxt  text texttexttext text text text text text text</p>
+	 	
+	 </div>
+	 <div class="question">
+	 		<h1>titre </h1>
+	 		<p>tesxt  text texttexttext text text text text text text</p>
+	 	
+	 </div>
+	 <div class="question">
+	 		<h1>titre </h1>
+	 		<p>tesxt  text texttexttext text text text text text text</p>
+	 	
+	 </div>
+	 <div class="question">
+	 		<h1>titre </h1>
+	 		<p>tesxt  text texttexttext text text text text text text</p>
+	 	
+	 </div>
+	 <div class="question">
+	 		<h1>titre </h1>
+	 		<p>tesxt  text texttexttext text text text text text text</p>
+	 	
+	 </div>
+	 	
+</div>
