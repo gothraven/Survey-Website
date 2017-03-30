3748019a1839f99770725c7f49056ff8d8da4b2d
Z. Safiy Errahmane <Z. Safiy Errahmane>
2017-03-30 19:20:42 +0200

    starting to leard Boostrap

--- index.php
+++ index.php
@@ -2,21 +2,61 @@
 $title = 'Welcome page';
 include("header.php");
     ?>
-
-    <h1>Page principale</h1>
     <div class="container">
-        <h1>My First Bootstrap Page</h1>
-        <p>This is some text.</p>
-        <p>Aller a <a href="home.php">Home</a></p>
+        <h1 class="text-primary">Page principale</h1>
+        <div class="jumbotron">
+            <table class="table table-hover">
+                <thead>
+                    <tr class="info">
+                        <th>Questionaires</th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>questionaire - 1</td>
+                        <td>20/02/2017</td>
+                        <td>Dr. Maha</td>
+                    </tr>
+                    <tr>
+                        <td>questionaire - 2</td>
+                        <td>22/03/2017</td>
+                        <td>Dr. Joe</td>
+                    </tr>
+                    <tr>
+                        <td>questionaire - 3</td>
+                        <td>30/01/2017</td>
+                        <td>Dr. Kevin</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="thumbnail">
+                    <p>test</p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="thumbnail">
+                    <p>text</p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="thumbnail">
+                    <p>text</p>
+                </div>
+            </div>
+        </div>
+        <dl>
+            <dt>QUESTIONAIRE - 1</dt>
+            <dd><pre><p class="text-info">question about health</p><kbd>ctrl + 1</kbd></pre></dd>
+            <dt>QUESTIONAIRE - 2</dt>
+            <dd><pre><p class="text-info">question about football</p><kbd>ctrl + 2</kbd></pre>
+            </dd>
+        </dl>
     </div>
-    <div class="container-fluid">
-        <h1>My First Bootstrap Page</h1>
-        <p>This is some text.</p>
-    </div>
-    <blockquote>
-        <p>For 50 years, WWF has been protecting the future of nature. The world's leading conservation organization, WWF works in 100 countries and is supported by 1.2 million members in the United States and close to 5 million globally.</p>
-        <footer>From WWF's website</footer>
-    </blockquote>
     <?php 
 include("footer.php");
 ?>
