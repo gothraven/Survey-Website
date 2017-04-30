73d7638321b031083295a8990462a3d45762c8de
Safiy Errahmane Zaghbane <safiy-errahmane.zaghbane@etu.u-pec.fr>
2017-04-30 06:44:08 +0200

    sumbiting answers ir ready

---
+++ user/sumbit_answers.php
@@ -0,0 +1,32 @@
+<?php 
+require("../auth/EtreAuthentifie.php");
+include("../db_config.php");
+include("../refCode.php");
+
+$refCode = RandomString(20);
+
+if(!empty($_POST)){
+    
+    $SQL = "INSERT INTO `saisie` (`sid`, `reference`) VALUES (NULL,?)";
+	$set = $db->prepare($SQL);
+    $result = $set->execute(array($refCode));
+    $sid = $db->lastInsertId();
+    
+    $SQL = "INSERT INTO `donnees` (`sid`, `cid`, `valeur`) VALUES (?,?,?)";
+    $set = $db->prepare($SQL);
+    foreach ($_POST as $key => $value) {
+       $result = $set->execute(array($sid,$key,$value));
+	}
+    
+    if(!$result){
+        echo "exit_failure";
+    }else{
+        echo "thank you for answering here is your referance code : '".$refCode."' you will need it for the future contacts";
+        //echo "<p>thank you for answering here is your referance code : <b>".$refCode."</b> you will need it for the future contacts</p>";
+    }
+    
+}else{
+    echo "exit_failure";
+}
+
+?>
--- user/user.php
+++ user/user.php
@@ -11,16 +11,16 @@ echo"<div class='well'>
     foreach($res as $row){
         echo"<button class='list-group-item questionnaire' id='$row[qid]'><h4><b>$row[intitule]</b></h4></button>";
     }
-echo "</div>";
-echo "</div>";
-echo "</div>";
-include("repond_questionnaire.php");
-include("done_answering.php");
-?>
+    echo "</div>";
+    echo "</div>";
+    echo "</div>";
+    include("repond_questionnaire.php");
+    include("done_answering.php");
+    ?>
 
     <script>
-        $(document).ready(function () {
-            $(document).on('click', '.questionnaire', function () {
+        $(document).ready(function() {
+            $(document).on('click', '.questionnaire', function() {
                 var id = $(this).attr("id");
                 $.ajax({
                     url: "user/getName_questionnaire.php",
@@ -28,7 +28,7 @@ include("done_answering.php");
                     data: {
                         qid: id
                     },
-                    success: function (data) {
+                    success: function(data) {
                         $('.modal-title').html(data);
                     }
                 });
@@ -38,20 +38,39 @@ include("done_answering.php");
                     data: {
                         qid: id
                     },
-                    success: function (data) {
+                    success: function(data) {
                         $('#questionnaire_here').html(data);
                         $('#repond-questionnaire').modal('show');
                     }
                 });
             });
         });
+
     </script>
     <script>
-        $(document).ready(function () {
-            $('#repond-questionnaire').on("submit", function (event) {
-                var refCode = "qsdqsd22ds2";
-                var thank = "Thank you for answering, here is your reference code ' " + refCode + " '.";
-                alert(thank);
+        $(document).ready(function() {
+            $('#repond-questionnaire').on('click', '#sumbit', function() {
+                $.ajax({
+                    url: "user/sumbit_answers.php",
+                    method: "POST",
+                    data: $('#questionnaire_form').serialize(),
+                    beforeSend: function() {
+                        $('#sumbit').val("Sumbiting...");
+                    },
+                    success: function(data) {
+                        $('#questionnaire_form')[0].reset();
+                        $('#repond-questionnaire').modal('hide');
+                        if (data == 'exit_failure') {
+                            alert("something wrong happened");
+                        } else {
+                            alert(data);
+                            //$('#reference_code_here').html(data);
+                            //$('#repond-questionnaire').modal('show');
+                        }
+                    }
+                });
+
             });
         });
+
     </script>
